5285784f9e7b78cb7fe4f9721f8c2b3bffd0b284
Mary Lin <marylin@chicagobooth.edu>
2017-05-03 13:09:04 -0500

    work from class

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get("/photos", {:controller => "photos", :action => "index"})
+
+  get("/photos/new",{:controller => "photos", :action => "new_form"}) #rails processes top to bottom
+  get("photos/create_photo",{:controller => "photos", :action => "create_row"})
+
+
+  get("/photos/:da_id",{:controller => "photos", :action => "show"})
+  get("/photos/:id/edit",{:controller => "photos", :action => "edit_form"})
+  get("photos/update_photo/:id",{:controller => "photos", :action => "update_row"})
+  get("photos/delete_photo/:id",{:controller => "photos", :action => "destroy_row"})
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
